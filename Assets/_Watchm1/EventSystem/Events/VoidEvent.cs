2a82a87f078e87a1ebc5fdaa4a02a0772e78cdb4
watchm1 <m.ali643452@gmail.com>
2022-08-03 13:52:02 +0300

    Event base system done

---
+++ Assets/_Watchm1/EventSystem/Events/VoidEvent.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+namespace _Watchm1.EventSystem.Events
+{
+    public struct Void
+    {
+        
+    }
+    [CreateAssetMenu(fileName = "Create void Event" ,menuName = "Watchm1Extension/GameEvents/VoidEvent")]
+    public class VoidEvent : BaseEvent<Void>
+    {
+        public void InvokeEvent() => SafeInvoke(item: new Void());
+    }
+}

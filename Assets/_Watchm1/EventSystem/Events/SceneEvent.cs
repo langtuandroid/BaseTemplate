dee49ebf3a512873082ef4f629d261d5148fad91
watchm1 <m.ali643452@gmail.com>
2022-08-07 01:51:52 +0300

    added some useful events

---
+++ Assets/_Watchm1/EventSystem/Events/SceneEvent.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace _Watchm1.EventSystem.Events
+{
+    [CreateAssetMenu(fileName = "Create void Event" ,menuName = "Watchm1Extension/SceneEvent")]
+    public class SceneEvent : BaseEvent<Scene>
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
